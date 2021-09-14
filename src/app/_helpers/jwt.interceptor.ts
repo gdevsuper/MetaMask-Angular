2f28962c70c9098f1e34225fbfc4815ba3baa30e
Vinay Gupta <vinaygupta771@gmail.com>
2021-09-14 11:39:56 +0530

    added changes

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -20,7 +20,8 @@ export class JwtInterceptor implements HttpInterceptor {
       request.url.includes('https://memeexhibit.com/api') ||
       request.url.includes('https://api.memeexhibit.com') ||
       request.url.includes('http://localhost:3001') ||
-      request.url.includes('http://localhost:3000')
+      request.url.includes('http://localhost:3000') ||
+      request.url.includes('http://147.182.187.164:3001')
     ) {
       // append headers
       this.headers = new HttpHeaders();
